f6bcf4fdd462eadaef96cb2977bac33858166e05
typewriter <takuyaxp@gmail.com>
2019-05-12 18:12:52 +0900

    Add item of areapeer

--- graphql/types/history.rb
+++ graphql/types/history.rb
@@ -2,8 +2,15 @@ require_relative 'base_enum'
 require_relative 'code'
 
 module Types
+  class AreaPeer < GraphQL::Schema::Object
+    field :id, Integer, null: false
+    field :peer, Integer, null: false
+  end
+
   class History < GraphQL::Schema::Object
     field :time, String, null: false
     field :code, Types::Code, null: false
+
+    field :areas, [Types::AreaPeer], null: true
   end
 end
--- models/whole.rb
+++ models/whole.rb
@@ -4,4 +4,6 @@ class Whole
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   store_in collection: "whole"
+
+  field :areas, type: Array
 end
